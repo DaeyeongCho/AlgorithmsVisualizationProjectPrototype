93276f6aad0967ac6e5df84342302872d7153fea
Cho Daeyeong <jdy0818@gmail.com>
2023-08-01 16:34:08 +0900

    11th commit

--- define.py
+++ define.py
@@ -1,11 +1,18 @@
 ## 윈도우 창 ##
 TITLE = "정렬/탐색 알고리즘 시뮬레이터"
-GEOMETRY = "1280x720"
+GEOMETRY = "1600x900"
+
+## 색상 ##
+RED = "red"
+YELLOW = "yellow"
+BLUE = "blue"
+WHITE = "white"
+BLACK = "black"
 
 ## General Menu 프레임 하위 위젯 ##
 RADIO_BUTTON_SELECT_ALGORITHM = [ "정렬 알고리즘", "탐색 알고리즘" ]
 
-SORT_ALGORITHMS = ["버블 정렬", "선택 정렬", "삽입 정렬", "병합 정렬", "힙 정렬", "퀵 정렬"]
+SORT_ALGORITHMS = ["버블 정렬", "선택 정렬", "삽입 정렬", "병합 정렬", "퀵 정렬", "힙 정렬"]
 SEARCH_ALGORITHMS = ["선형 탐색", "이진 탐색"]
 
 ADVANCED_MENU = "고급"
@@ -18,8 +25,8 @@ SPIN_BOX_WIDTH = 10
 DATA_SIZE = "데이터 크기"
 DATA_SIZE_DEFAULT = 100
 DATA_SIZE_MIN = 10
-DATA_SIZE_MAX = 300
-DATA_SIZE_INCREMENT = 1
+DATA_SIZE_MAX = 500
+DATA_SIZE_INCREMENT = 10
 
 SPEED_LIMIT = "속도 제한(s)"
 SPEED_LIMIT_DEFAULT = 0.000
@@ -28,10 +35,12 @@ SPEED_LIMIT_MAX = 1.000
 SPEED_LIMIT_INCREMENT = 0.001
 
 SHUFFLE_TIMES = "섞는 횟수(회)"
-SHUFFLE_TIMES_DEFAULT = 100
+SHUFFLE_TIMES_DEFAULT = 1000
 SHUFFLE_TIMES_MIN = 0
 SHUFFLE_TIMES_MAX = 100000
 SHUFFLE_TIMES_INCREMENT = 100
+
+SHUFFLE_VISIBLE = "섞기 모션 보이기"
 
 SEARCH_VALUE = "탐색 값(0:랜덤)"
 SEARCH_VALUE_DEFAULT = 0
@@ -39,15 +48,14 @@ SEARCH_VALUE_MIN = 0
 SEARCH_VALUE_INCREMENT = 1
 
 ## Simulation 프레임 하위 위젯 ##
-CANVAS_WIDTH = 1000
-CANVAS_HEIGHT = 500
+CANVAS_WIDTH = 1500
+CANVAS_HEIGHT = 700
 CANVAS_COLOR = "white"
 
 LABEL_HINT = ["주목", "비교", "픽스 됨", "탐색 값 : ", "-"]
-LABEL_HINT_COLOR = ["red", "blue", "yellow", "black"]
 
 LABEL_STATE = ["상태 : ", "---", "섞는 중...", "정렬 중...", "탐색 중...","섞기 완료!" , "정렬 완료!", "탐색 완료!"]
 
-ELAPSED_TIME = ["시간(s:ms)", "-:---"]
+ELAPSED_TIME = ["시간(m:s.ms)", "--:--.---"]
 
 END_BUTTONS = ["정지", "다시하기", "닫기"]
--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import ctypes
 import random
 import threading
 import time
@@ -28,6 +29,8 @@ def main():
     doubleVarSpeedLimit.set(SPEED_LIMIT_DEFAULT)
     intVarShuffleTimes = IntVar()
     intVarShuffleTimes.set(SHUFFLE_TIMES_DEFAULT)
+    boolVarShuffleVisible = BooleanVar()
+    boolVarShuffleVisible.set(True)
     intVarSearchValue = IntVar()
     intVarSearchValue.set(SEARCH_VALUE_DEFAULT)
 
@@ -62,17 +65,19 @@ def main():
     labelShuffleTimes = Label(frameAdvancedMenu, text=SHUFFLE_TIMES)
     spinboxShuffleTimes = Spinbox(frameAdvancedMenu, from_=SHUFFLE_TIMES_MIN, to=SHUFFLE_TIMES_MAX, increment=SHUFFLE_TIMES_INCREMENT, textvariable=intVarShuffleTimes, width=SPIN_BOX_WIDTH)
 
+    checkbuttonShuffleVisible = ttk.Checkbutton(frameAdvancedMenu, text=SHUFFLE_VISIBLE, variable=boolVarShuffleVisible)
+
     labelSearchValue = Label(frameAdvancedMenu, text=SEARCH_VALUE)
     spinboxSearchValue = Spinbox(frameAdvancedMenu, from_=SEARCH_VALUE_MIN, to=int(intVarSpinboxDataSize.get()), increment=SEARCH_VALUE_INCREMENT, textvariable=intVarSearchValue, width=SPIN_BOX_WIDTH)
 
     # Simulation 프레임 하위 위젯 정의
     canvas = Canvas(frameSimulation, width=CANVAS_WIDTH, height=CANVAS_HEIGHT, background=CANVAS_COLOR)
 
-    labelHint = [ Label(frameSimulationHint, text=LABEL_HINT[0], foreground=LABEL_HINT_COLOR[0]),
-                  Label(frameSimulationHint, text=LABEL_HINT[1], foreground=LABEL_HINT_COLOR[1]),
-                  Label(frameSimulationHint, text=LABEL_HINT[2], foreground=LABEL_HINT_COLOR[2]), 
-                  Label(frameSimulationHint, text=LABEL_HINT[3], foreground=LABEL_HINT_COLOR[3]), 
-                  Label(frameSimulationHint, text=LABEL_HINT[4], foreground=LABEL_HINT_COLOR[3]), ]
+    labelHint = [ Label(frameSimulationHint, text=LABEL_HINT[0], foreground=RED),
+                  Label(frameSimulationHint, text=LABEL_HINT[1], foreground=BLUE),
+                  Label(frameSimulationHint, text=LABEL_HINT[2], foreground=YELLOW), 
+                  Label(frameSimulationHint, text=LABEL_HINT[3], foreground=BLACK), 
+                  Label(frameSimulationHint, text=LABEL_HINT[4], foreground=BLACK), ]
     
     labelState = [ Label(frameSimulationState, text=LABEL_STATE[0]),
                    Label(frameSimulationState, text=LABEL_STATE[1]) ]
@@ -95,6 +100,7 @@ def main():
             comboboxSelectAlgorithm.set(SORT_ALGORITHMS[0])
             labelShuffleTimes.pack(side=LEFT, padx=(10, 0))
             spinboxShuffleTimes.pack(side=LEFT, padx=(2, 0))
+            checkbuttonShuffleVisible.pack(side=LEFT, padx=(10, 0))
             labelSearchValue.pack_forget()
             spinboxSearchValue.pack_forget()
         else:
@@ -102,6 +108,7 @@ def main():
             comboboxSelectAlgorithm.set(SEARCH_ALGORITHMS[0])
             labelShuffleTimes.pack_forget()
             spinboxShuffleTimes.pack_forget()
+            checkbuttonShuffleVisible.pack_forget()
             labelSearchValue.pack(side=LEFT, padx=(10, 0))
             spinboxSearchValue.pack(side=LEFT, padx=(2, 0))
 
@@ -117,6 +124,7 @@ def main():
         global dataSize
         global speedLimit
         global shuffleTime
+        global shuffleVisible
         global searchValue
         global remindSearchValue
         global data
@@ -136,21 +144,13 @@ def main():
         dataSize = intVarSpinboxDataSize.get()
         speedLimit = doubleVarSpeedLimit.get()
         shuffleTime = intVarShuffleTimes.get()
+        shuffleVisible = boolVarShuffleVisible.get()
         searchValue = intVarSearchValue.get()
         remindSearchValue = searchValue
 
-        radioButtonSelectAlgorithm[0].config(state="disabled")
-        radioButtonSelectAlgorithm[1].config(state="disabled")
-        comboboxSelectAlgorithm.config(state="disabled")
-        checkbuttonAdvancedMenu.config(state="disabled")
         buttonStart.config(state="disabled")
-        spinboxDataSize.config(state="disabled")
-        spinboxSpeedLimit.config(state="disabled")
-        spinboxShuffleTimes.config(state="disabled")
-        spinboxSearchValue.config(state="disabled")
 
         createSticks()
-
         startSimulationThread()
 
 
@@ -160,6 +160,7 @@ def main():
 
     # Simulation 프로그램
     def buttonStopFunc():
+        
         pass
 
     def buttonReplayFunc():
@@ -172,6 +173,7 @@ def main():
         labelState[1].config(text=LABEL_STATE[1])
         labelElapsedTime[1].config(text=ELAPSED_TIME[1])
         labelHint[4].configure(text=LABEL_HINT[4])
+        buttonStart.config(state="disabled")
         buttonEnd[1].config(state="disabled")
         buttonEnd[2].config(state="disabled")
 
@@ -201,7 +203,7 @@ def main():
         if timerRunning:
             global timerStartTime
             timerNowTime = int((time.time() - timerStartTime) * 1000)
-            labelElapsedTime[1].config(text=str(timerNowTime // 1000) + ":" + str(timerNowTime % 1000).zfill(3))
+            labelElapsedTime[1].config(text=str(timerNowTime // 60000).zfill(2) + ":" + str((timerNowTime % 60000) // 1000).zfill(2) + "." + str(timerNowTime % 1000).zfill(3))
         window.after(1, timerFunc)
 
 
@@ -210,8 +212,13 @@ def main():
     # canvas의 data[데이터 값, 막대 그래프] 리스트 생성 및 순서에 따라 재배치
     def createSticks():
         for i in range(dataSize):
-            data.append([i + 1, canvas.create_rectangle(0, 0, CANVAS_WIDTH / dataSize, CANVAS_HEIGHT * (i + 1) / dataSize, fill="white")])
+            data.append([i + 1, canvas.create_rectangle(0, 0, CANVAS_WIDTH / dataSize, CANVAS_HEIGHT * (i + 1) / dataSize, fill=WHITE)])
 
+        for i in data:
+            canvas.moveto(i[1], CANVAS_WIDTH * data.index(i) / dataSize + 1, CANVAS_HEIGHT - CANVAS_HEIGHT * i[0] / dataSize + 1)
+
+    #canvas의 data[데이터 값, 막대 그래프] 리스트 순서에 따라 재배치
+    def relocationStick():
         for i in data:
             canvas.moveto(i[1], CANVAS_WIDTH * data.index(i) / dataSize + 1, CANVAS_HEIGHT - CANVAS_HEIGHT * i[0] / dataSize + 1)
 
@@ -231,7 +238,11 @@ def main():
 
         if sortOrSearchAlgorithm == 0: # 정렬 알고리즘
             labelState[1].config(text=LABEL_STATE[2])
-            shuffleStick()
+            if shuffleVisible:
+                shuffleStick()
+            else:
+                shuffleStickNotVisible()
+                relocationStick()
             labelState[1].config(text=LABEL_STATE[5])
             time.sleep(1)
             labelState[1].config(text=LABEL_STATE[3])
@@ -241,6 +252,16 @@ def main():
             if algorithm == SORT_ALGORITHMS[0]:
                 bubbleSort()
             elif algorithm == SORT_ALGORITHMS[1]:
+                selectSort()
+            elif algorithm == SORT_ALGORITHMS[2]:
+                insertSort()
+            elif algorithm == SORT_ALGORITHMS[3]:
+                mergeSort()
+            elif algorithm == SORT_ALGORITHMS[4]:
+                quickSort()
+            elif algorithm == SORT_ALGORITHMS[5]:
+                pass
+            else:
                 pass
 
             timerRunning = False
@@ -256,6 +277,8 @@ def main():
             if algorithm == SEARCH_ALGORITHMS[0]:
                 linearSearch()
             elif algorithm == SEARCH_ALGORITHMS[1]:
+                binarySearch()
+            else:
                 pass
 
             timerRunning = False
@@ -276,33 +299,37 @@ def main():
 
 
     # 두 막대 교환 함수
-    def exchangePairStick(attentionStickIndex, compareStickIndex, attentionStickColor = "red", compareStickColor = "blue"):
-        canvas.itemconfig(data[attentionStickIndex][1], fill=attentionStickColor)
-        canvas.itemconfig(data[compareStickIndex][1], fill=compareStickColor)
-
-        if speedLimit != 0:
-            time.sleep(speedLimit / 2)
-        data[attentionStickIndex], data[compareStickIndex] = data[compareStickIndex], data[attentionStickIndex]
-        canvas.moveto(data[attentionStickIndex][1], CANVAS_WIDTH * attentionStickIndex / dataSize + 1, CANVAS_HEIGHT - CANVAS_HEIGHT * data[attentionStickIndex][0] / dataSize + 1)
-        canvas.moveto(data[compareStickIndex][1], CANVAS_WIDTH * compareStickIndex / dataSize + 1, CANVAS_HEIGHT - CANVAS_HEIGHT * data[compareStickIndex][0] / dataSize + 1)
-
-        if speedLimit != 0:
-            time.sleep(speedLimit / 2)
-        canvas.itemconfig(data[attentionStickIndex][1], fill="white")
-        canvas.itemconfig(data[compareStickIndex][1], fill="white")
-
-    # 두 막대를 교환하지 않고 색상만 표현하는 함수
-    def notExchangePairStick(attentionStickIndex, compareStickIndex, attentionStickColor = "red", compareStickColor = "blue"):
+    def exchangePairStick(attentionStickIndex, compareStickIndex, attentionStickColor = RED, compareStickColor = BLUE):
         canvas.itemconfig(data[attentionStickIndex][1], fill=attentionStickColor)
         canvas.itemconfig(data[compareStickIndex][1], fill=compareStickColor)
 
         if speedLimit != 0:
             time.sleep(speedLimit)
-        canvas.itemconfig(data[attentionStickIndex][1], fill="white")
-        canvas.itemconfig(data[compareStickIndex][1], fill="white")
+        data[attentionStickIndex], data[compareStickIndex] = data[compareStickIndex], data[attentionStickIndex]
+        canvas.moveto(data[attentionStickIndex][1], CANVAS_WIDTH * attentionStickIndex / dataSize + 1, CANVAS_HEIGHT - CANVAS_HEIGHT * data[attentionStickIndex][0] / dataSize + 1)
+        canvas.moveto(data[compareStickIndex][1], CANVAS_WIDTH * compareStickIndex / dataSize + 1, CANVAS_HEIGHT - CANVAS_HEIGHT * data[compareStickIndex][0] / dataSize + 1)
 
-    # 막대 색상 변경
+        if speedLimit != 0:
+            time.sleep(speedLimit)
+        canvas.itemconfig(data[attentionStickIndex][1], fill=WHITE)
+        canvas.itemconfig(data[compareStickIndex][1], fill=WHITE)
+
+    # 두 막대를 교환하지 않고 색상만 표현하는 함수
+    def notExchangePairStick(attentionStickIndex, compareStickIndex, attentionStickColor = RED, compareStickColor = BLUE):
+        canvas.itemconfig(data[attentionStickIndex][1], fill=attentionStickColor)
+        canvas.itemconfig(data[compareStickIndex][1], fill=compareStickColor)
+
+        if speedLimit != 0:
+            time.sleep(speedLimit)
+        canvas.itemconfig(data[attentionStickIndex][1], fill=WHITE)
+        canvas.itemconfig(data[compareStickIndex][1], fill=WHITE)
+
+    # 막대 색상 변경 (딜레이 없음)
     def changeColor(stickIndex, color):
+        canvas.itemconfig(data[stickIndex][1], fill=color)
+
+    # 막대 색상 변경 (딜레이 있음)
+    def changeColorDelay(stickIndex, color):
         canvas.itemconfig(data[stickIndex][1], fill=color)
 
         if speedLimit != 0:
@@ -318,7 +345,21 @@ def main():
 
         for i in range(shuffleTime):
             drawTwoStick = random.sample(data, 2)
-            exchangePairStick(drawTwoStick[0][0] - 1, drawTwoStick[1][0] - 1, "red", "red")
+            exchangePairStick(drawTwoStick[0][0] - 1, drawTwoStick[1][0] - 1, RED, RED)
+
+        speedLimit = tempSpeedLimitMemory
+
+    # 정렬 선택 시 막대 섞기 함수
+    def shuffleStickNotVisible():
+        global speedLimit
+    
+        drawTwoStick = []
+        tempSpeedLimitMemory = speedLimit
+        speedLimit = 0.0
+
+        for i in range(shuffleTime):
+            drawTwoStick = random.sample(data, 2)
+            data[drawTwoStick[0][0] - 1], data[drawTwoStick[1][0] - 1] = data[drawTwoStick[1][0] - 1], data[drawTwoStick[0][0] - 1]
 
         speedLimit = tempSpeedLimitMemory
 
@@ -333,17 +374,74 @@ def main():
                 else:
                     continue
 
-            changeColor(i, "yellow")
+            changeColor(i, YELLOW)
+
+    def selectSort():
+        minimumIndex = 0
+        minimumValue = data[0][0]
+
+        for i in range(dataSize):
+            minimumIndex = i
+            minimumValue = data[i][0]
+            for y in range(i + 1, dataSize):
+                notExchangePairStick(minimumIndex, y)
+                if minimumValue > data[y][0]:
+                    minimumIndex = y
+                    minimumValue = data[y][0]
+            exchangePairStick(i, minimumIndex, RED, RED)
+            changeColor(i, YELLOW)
+
+    def insertSort():
+        for i in range(1, dataSize):
+            for y in range(i, 0, -1):
+                if data[y][0] < data[y - 1][0]:
+                    exchangePairStick(y, y - 1)
+                    changeColor(y, YELLOW)
+                    changeColor(y - 1, YELLOW)
+                else:
+                    notExchangePairStick(y, y - 1)
+                    changeColor(y, YELLOW)
+                    changeColor(y - 1, YELLOW)
+                    break
+
+    def mergeSort():
+        pass
+
+    def quickSort():
+        pass
+
 
 ## ====================== 탐색 알고리즘 ====================== ##
     def linearSearch():
         for i in data:
-            changeColor(i[0] - 1, "red")
+            changeColorDelay(i[0] - 1, RED)
             if i[0] == searchValue:
-                changeColor(i[0] - 1, "yellow")
+                changeColor(i[0] - 1, YELLOW)
                 break
             else:
-                changeColor(i[0] - 1, "white")
+                changeColor(i[0] - 1, WHITE)
+
+    def binarySearch():
+        center = 0
+        left = 0
+        right = dataSize - 1
+
+        while True:
+            center = left + (right - left) // 2
+            changeColor(left, BLUE)
+            changeColor(right, BLUE)
+            changeColorDelay(center, RED)
+            if data[center][0] == searchValue:
+                break
+            elif data[center][0] > searchValue:
+                changeColor(right, WHITE)
+                right = center - 1
+            else:
+                changeColor(left, WHITE)
+                left = center + 1
+            changeColor(center, WHITE)
+        changeColor(center, YELLOW)
+
 
 ## ====================== 배치 ====================== ##
 
@@ -366,6 +464,7 @@ def main():
     spinboxSpeedLimit.pack(side=LEFT, padx=(2, 0))
     labelShuffleTimes.pack(side=LEFT, padx=(10, 0))
     spinboxShuffleTimes.pack(side=LEFT, padx=(2, 0))
+    checkbuttonShuffleVisible.pack(side=LEFT, padx=(10, 0))
     labelSearchValue.pack_forget()
     spinboxSearchValue.pack_forget()
 
@@ -405,6 +504,7 @@ algorithm = ""
 dataSize = 0
 speedLimit = 0.0
 shuffleTime = 0
+shuffleVisible = False
 searchValue = 0
 remindSearchValue = 0
 
